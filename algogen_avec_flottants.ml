7dfe90559c76c505dbfa8d568f4508c407da37cb
antoineenac <84006843+antoineenac@users.noreply.github.com>
2021-11-30 14:08:54 +0100

    Add files via upload

--- algogen_avec_flottants.ml
+++ algogen_avec_flottants.ml
@@ -3,6 +3,20 @@ open Random;;
 Random.self_init;;
 
 
+let deep_copy_pop = fun pop ->
+	let taille_pop = Array.length pop in
+	let taille_individu = Array.length pop.(0) in
+	let copy = Array.make_matrix taille_pop taille_individu pop.(0).(0) in
+	for k = 0 to taille_pop-1 do
+		for i = 0 to taille_individu-1 do
+			copy.(k).(i)<- pop.(k).(i)
+		done;
+	done;
+	copy;;
+
+
+let aaa=[| [|1;2;3|];[|4;5;6|] |];;
+let bbb= deep_copy_pop aaa;;
 
 let fmin_un_terme = fun tab ->
 	let m = ref 0 in 
@@ -128,14 +142,14 @@ let one_step_evol_diff_float = fun pop fobj cr f ->
     pop;;
 
 let evol_diff = fun pop gen_max fobj cr f ->
-    let aux_pop = ref pop in
+    let aux_pop = ref (deep_copy_pop pop) in
     for k=0 to gen_max do
         aux_pop := one_step_evol_diff (!aux_pop) fobj cr f;
     done;
     !aux_pop;;
     
 let evol_diff_float = fun pop gen_max fobj cr f ->
-    let aux_pop = ref pop in
+    let aux_pop = ref (deep_copy_pop pop) in
     for k=0 to gen_max do
         aux_pop := one_step_evol_diff_float (!aux_pop) fobj cr f;
     done;
@@ -163,28 +177,51 @@ let make_pop_test_float = fun taille_pop taille_individu ->
 	
 let pop_test = make_pop_test 10 10;;
 
+let copy_pop_test = deep_copy_pop pop_test;;
+
 let test = evol_diff pop_test 1000 close_zero 5 2;;
 
 let pop_test_float = make_pop_test_float 10 10;;
 
 let test = evol_diff_float pop_test_float 1000 close_zero_float 5 0.1;;
 
-(* let optim_cr_f = fun nb_gen taille_pop taille_individu ->
-	let pop = ref make_pop_test_float taille_pop taille_individu in
-	let score = ref 
-	let cr = ref 0.1 in
-	let bestcr = ref 0.1 in 
-	let f = ref 0.1 in
-	let bestf = ref 0.1 in 
-	for pas_cr = 0 to 9 do 
-		cr := !cr +. 0.1*.pas_cr;
-		for pas_f = 0 to 19 do
-			f := !f +. 0.1*.pas_f;
-			let aux = evol_diff_float pop 1000 close_zero_float !cr !f in *)
+
+let best_individu = fun pop fobj ->
+	let nb_individu = Array.length pop in 
+	let min = ref (fobj pop.(0)) in
+	let indice_min = ref 0 in 
+	for k=1 to nb_individu-1 do 
+		let aux = (fobj pop.(k)) in
+		if aux <(!min) then begin min := aux; indice_min := k end
+	done;
+	!indice_min,!min;;
+
+let optim_cr_f = fun nb_gen taille_pop taille_individu fobj ->
+	let pop = ref (make_pop_test_float taille_pop taille_individu) in
+	let score = ref infinity in
+	let cr = ref 0 in
+	let bestcr = ref 0 in 
+	let f = ref 0.0 in
+	let bestf = ref 0.0 in 
+	for pas_cr = 0 to 100 do 
+		cr := !cr + 1 ;
+		f := 0.0;
+		for pas_f = 0 to 20 do
+			f := !f +. 0.1;
+			let aux = evol_diff_float !pop nb_gen fobj !cr !f in 
+			let couple = best_individu aux fobj in
+			let meilleur_score = snd couple in
+			if meilleur_score < !score then begin
+				score := meilleur_score;
+				bestcr := !cr;
+				bestf := !f;
+			end
+		done;
+		Printf.printf "%d \n" !cr;
+	done;
+	!bestcr,!bestf,!score;;
+
+let test_optim = optim_cr_f 1000 10 10 close_zero_float;;			
 			
 		
-
-
-
-
 
